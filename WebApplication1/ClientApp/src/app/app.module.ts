40aaab99ee733c9307a5805a6c4473b7fdb26ce7
isidoreduc <isidoreducasse79@gmail.com>
2018-12-02 07:29:12 +0100

    routing and adding some empty components for the nav area

--- WebApplication1/ClientApp/src/app/app.module.ts
+++ WebApplication1/ClientApp/src/app/app.module.ts
@@ -10,21 +10,27 @@ import { HomeComponent } from './components/home/home.component';
 import { QuizListComponent } from './components/quiz/quiz-list.component';
 import { QuizService } from './services/quiz.service';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent, QuizListComponent, QuizComponent
+    AppComponent, NavMenuComponent, HomeComponent, QuizListComponent,
+    QuizComponent, AboutComponent, LoginComponent, PageNotFoundComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' }
-     
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'quiz/:id', component: QuizComponent }, //
+      { path: 'about', component: AboutComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '**', component: PageNotFoundComponent } // global fallback, should be last or the other after it will never execute
     ])
   ],
   providers: [QuizService],
